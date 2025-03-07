3093803b5ed897ad66a6dac46c5da5a220e25e5c
Lucas de Lima <54589831+AnimisAle@users.noreply.github.com>
2025-03-06 22:47:26 -0300

    Cast

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ print('2 - Listar Restaurante')
 print('3 - Ativar Restaurante')
 print('4 - Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ') 
+opcao_escolhida = int(input('Escolha uma opção: ')) 
 print(f'Você escolheu a opção: {opcao_escolhida}.')
 
 print(type(opcao_escolhida))
